8903f641b133a18f576cf868dfb1acecd631b00e
Darshan Datta <darshandatta.10@gmail.com>
2020-09-29 00:45:25 +0530

    added msg if no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,21 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = ({ ingredients }) => {
-  // transform to array
-  const transformedIngredients = Object.keys(ingredients).map((igkey) => {
-    return [...Array(ingredients[igkey])].map((_, i) => (
-      <BurgerIngredient key={igkey + i} type={igkey} />
-    ));
-  });
+  // transform to array or return p if no ingredients
+  let transformedIngredients = Object.keys(ingredients)
+    .map((igkey) => {
+      return [...Array(ingredients[igkey])].map((_, i) => (
+        <BurgerIngredient key={igkey + i} type={igkey} />
+      ));
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  // msg if no ingredient
+  if (!transformedIngredients.length) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
