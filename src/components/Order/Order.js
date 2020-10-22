22ec9656a80ff14578248874a860b0696cf5ac78
Darshan Datta <darshandatta.10@gmail.com>
2020-10-22 20:46:54 +0530

    created a page (orders) to show all orders

---
+++ src/components/Order/Order.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./Order.module.css";
+
+const Order = (props) => {
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: Salad (1)</p>
+      <p>
+        Price: <strong>Rs 100.00</strong>
+      </p>
+    </div>
+  );
+};
+
+export default Order;
