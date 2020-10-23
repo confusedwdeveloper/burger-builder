4a30bdaa678ddfbec42d3e31049d239095b91744
Darshan Datta <darshandatta.10@gmail.com>
2020-10-23 13:05:40 +0530

    enabled fetching orders in Orders page

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
 import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
-export default class Orders extends Component {
+class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then((res) => {
+        const orders = [];
+        for (const key in res.data) {
+          // this approach to save the key or we could have used Object.values
+          orders.push({
+            ...res.data[key],
+            id: key,
+          });
+        }
+        this.setState({ loading: false, orders });
+      })
+      .catch((err) => {
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div>
@@ -11,3 +37,5 @@ export default class Orders extends Component {
     );
   }
 }
+
+export default withErrorHandler(Orders, axios);
