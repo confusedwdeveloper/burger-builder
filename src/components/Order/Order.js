85807a02ce2fb5090354a5ea5afd7a452af7bb1c
Darshan Datta <darshandatta.10@gmail.com>
2020-10-23 14:25:52 +0530

    orders are dislayed based on fetched data

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,39 @@ import React from "react";
 import classes from "./Order.module.css";
 
 const Order = (props) => {
+  const ingredients = Object.entries(props.ingredients).map(
+    ([ingredient, val]) => ({
+      name: ingredient,
+      amount: val,
+    })
+  );
+  // filter out ingredients which don't have value
+  let filteredIngredients = ingredients.filter((ig) => ig.amount !== 0);
+  // now sort by bigger amount to smaller amount
+  filteredIngredients.sort((a, b) => {
+    return b.amount - a.amount;
+  });
+
+  const output = filteredIngredients.map((ig) => (
+    <span
+      style={{
+        textTransform: "capitalize",
+        display: "inline-block",
+        margin: "0 8px",
+        border: "1px solid #ccc",
+        padding: "5px",
+      }}
+      key={ig.name}
+    >
+      {ig.name} ({ig.amount})
+    </span>
+  ));
+
   return (
     <div className={classes.Order}>
-      <p>Ingredients: Salad (1)</p>
+      <p>Ingredients: {output}</p>
       <p>
-        Price: <strong>Rs 100.00</strong>
+        Price: <strong>Rs {props.price.toFixed(2)}</strong>
       </p>
     </div>
   );
